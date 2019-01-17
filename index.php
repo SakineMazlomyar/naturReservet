1d4ed94e69d2c17b2b8cd1b4aa871ec9fd8f175d
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-17 11:53:06 +0100

    adddedFormen

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<h1>Ange total djur du söker!</h1><br>
+    <form id="ajax" method="post" action="./results.php">
+    <label>Apa</label>
+    <input type="number" name='animalNumber1'>
+
+    <label>Giraff</label>
+    <input type="number" name='animalNumber2'>
+    
+    <label>Tiger</label>
+    <input type="number" name='animalNumber3'>
+
+    <label>Kokosnött</label>
+    <input type="number" name='animalNumber4'>
+    <input type="submit">
+    </form>
+</body>
+</html>
