04566abc3163a17b0652654f051909f64df1b14b
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-18 13:19:15 +0100

    renamed

---
+++ result/result.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Animal{
+    abstract function makeSound();
+
+
+}
+
+include("../superclasses/apaSuperclass.php");
+include("../superclasses/giraffSuperclass.php");
+include("../superclasses/tigerSuperclass.php");
+include("../superclasses/kokosnottSuperclass.php");
+
+
+
+?>
