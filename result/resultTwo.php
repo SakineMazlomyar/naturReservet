e1472e607cea3ca19fe0f5b86a9e43136b195402
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-18 10:27:44 +0100

    madeShortCode

---
+++ result/resultTwo.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Animal{
+    abstract function makeSound();
+
+}
+
+include("../superclasses/apaSuperclass.php");
+include("../superclasses/giraffSuperclass.php");
+include("../superclasses/tigerSuperclass.php");
+include("../superclasses/kokosnottSuperclass.php");
+
+
+
+?>
