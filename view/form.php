4a00c0d0fdc2e23d5bfdf8ec72bfac4eacaa0dd5
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-18 13:15:21 +0100

    resturcutred

---
+++ view/form.php
@@ -0,0 +1,18 @@
+
+
+
+
+<!--     <form id="ajax" method="post" action="./result/resultTwo.php">
+    <label>Apa</label>
+    <input type="number" name='animalNumber1'>
+
+    <label>Giraff</label>
+    <input type="number" name='animalNumber2'>
+    
+    <label>Tiger</label>
+    <input type="number" name='animalNumber3'>
+
+    <label>Kokosnött</label>
+    <input type="number" name='animalNumber4'>
+    <input type="submit" name='subitAnimal' >
+    </form> -->
