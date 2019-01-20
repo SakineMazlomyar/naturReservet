c8eabeab62f104e2001315d0882f8334b7812498
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-20 20:42:36 +0100

    reconstructed

--- superclasses/tigerSuperclass.php
+++ superclasses/tigerSuperclass.php
@@ -1,28 +1,16 @@
 <?php
-
 class Tiger extends Animal{
-    protected $picture = "";
-    public $animalNmber = "";
-    
-
     function __construct($picture, $animalNmbers){
-        $this->picture = $picture;
-        $this->animalNmber = $animalNmbers;
+        parent::__construct($picture, $animalNmbers);
     
-
     }
-    public function getPicture(){
-        for($i=0; $i<$this->animalNmber; $i++){
-            echo $this->picture;
-        }
-
+    public function getPicture() {
+        return parent::getPicture();
     }
 
     function makeSound(){
         return "Det här är tigers djud";
     }
-
-
 }
 $tiger = new Tiger("<img style='width: 30%;' src='../pictures/tiger.jpg'>", $_POST['Tigrar']);
 echo $tiger->getPicture();
