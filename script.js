bcd327eba655266dec3749662d3252e75965c3d8
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-21 09:45:34 +0100

    addedScript

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function playMonkeySound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","../sounds/...mp3" )
+    src.setAttribute("type", "audio/mpeg")
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    alert("Monkey")
+}
+function playGiraffSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","../sounds/...mp3" )
+    src.setAttribute("type", "audio/mpeg")
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    alert("Giraff")
+}
+function playKokosnottSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","../sounds/...mp3" )
+    src.setAttribute("type", "audio/mpeg")
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    alert("Kokosnott")
+}
+function playTigerSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","../sounds/...mp3" )
+    src.setAttribute("type", "audio/mpeg")
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    alert("Tiger")
+}
