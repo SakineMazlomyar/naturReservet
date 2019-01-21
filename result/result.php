9bed855c4ea2ac979d7d084ac353a7fd927ddf96
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-21 10:41:59 +0100

    restructed

--- result/result.php
+++ result/result.php
@@ -1,29 +1,23 @@
+<?php include("../view/headSubClasses.php")?>
 <?php
-
-//header("Content-Type: application/json");
-abstract class Animal{
-    protected $animalPictures= "";
-    protected $animalNmbers = "";
-
-    public function getPicture(){
-        for($i = 0; $i <$this->animalNmbers; $i++){
-            echo $this->animalPictures;
-        }
-        
-    }
-
-    function __construct($animalPictures, $animalNmbers){
-        $this->animalNmbers = $animalNmbers;
-        $this->animalPictures = $animalPictures;
-    }
-
-}
-
-include("../superclasses/apaSuperclass.php");
-include("../superclasses/giraffSuperclass.php");
-include("../superclasses/tigerSuperclass.php");
-include("../superclasses/kokosnottSuperclass.php");
+include("../classes/animalClass.php");
+include("../classes/apaClass.php");
+include("../classes/giraffClass.php");
+include("../classes/kokosnottClass.php");
+include("../classes/tigerClass.php");
 
 
+$apa = new Apor("<img style='width: 30%;' src='../pictures/apa.jpg' onclick=playMonkeySound()>", $_POST['Apor']);
+echo $apa->getPicture();
+$graff = new Giraffer("<img style='width: 30%;' src='../pictures/giraff.jpg' onclick=playGiraffSound()>", $_POST['Giraffer']);
+echo $graff->getPicture();
+$kokosnott = new Kokosnotter("<img style='width: 30%;' src='../pictures/kokosnott.png' onclick=playKokosnottSound()>", $_POST['Kokosnottrar']);
+echo $kokosnott->getPicture();
+$tiger = new Tiger("<img style='width: 30%;' src='../pictures/tiger.jpg' onclick=playTigerSound()>", $_POST['Tigrar']);
+echo $tiger->getPicture();
 
 ?>
+<body>
+    
+</body>
+</html>
