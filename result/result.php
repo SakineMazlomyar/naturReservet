e57a6981ed756b7d0b766ffbb81943d526460970
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-21 20:00:26 +0100

    renamed

--- result/result.php
+++ result/result.php
@@ -1,5 +1,6 @@
 <?php include("../view/headSubClasses.php")?>
 <?php
+session_start();
 include("../classes/animalClass.php");
 include("../classes/apaClass.php");
 include("../classes/giraffClass.php");
@@ -7,15 +8,18 @@ include("../classes/kokosnottClass.php");
 include("../classes/tigerClass.php");
 
 
-$apa = new Apor("<img style='width: 30%;' src='../pictures/apa.jpg' onclick=playMonkeySound()>", $_POST['Apor']);
+$apa = new Apa("<img style='width: 30%;' src='../pictures/apa.jpg' onclick=playMonkeySound()>", $_POST['Apa']);
 echo $apa->getPicture();
-$graff = new Giraffer("<img style='width: 30%;' src='../pictures/giraff.jpg' onclick=playGiraffSound()>", $_POST['Giraffer']);
+$graff = new Giraff("<img style='width: 30%;' src='../pictures/giraff.jpg' onclick=playGiraffSound()>", $_POST['Giraff']);
 echo $graff->getPicture();
-$kokosnott = new Kokosnotter("<img style='width: 30%;' src='../pictures/kokosnott.png' onclick=playKokosnottSound()>", $_POST['Kokosnottrar']);
+$kokosnott = new Kokosnott("<img style='width: 30%;' src='../pictures/kokosnott.png' onclick=playKokosnottSound()>", $_POST['Kokosnott']);
 echo $kokosnott->getPicture();
-$tiger = new Tiger("<img style='width: 30%;' src='../pictures/tiger.jpg' onclick=playTigerSound()>", $_POST['Tigrar']);
+$tiger = new Tiger("<img style='width: 30%;' src='../pictures/tiger.jpg' onclick=playTigerSound()>", $_POST['Tiger']);
 echo $tiger->getPicture();
-
+$_SESSION['apa'] = $_POST['Apa'];
+$_SESSION['giraff'] = $_POST['Giraff'];
+$_SESSION['kokosnott'] = $_POST['Kokosnott'];
+$_SESSION['tiger'] = $_POST['Tiger'];
 ?>
 <body>
     
