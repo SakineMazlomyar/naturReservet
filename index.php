6157b06fcfc1270783b828bf529bcf1884528b2d
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-22 09:05:52 +0100

    workingOnIndex

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
 <?php
 session_start();
-
 if(isset($_SESSION['apa'])){
-   
     header('Location:result/result.php');
-    
-} elseif (isset($_SESSION['tiger'])){
+    exit;
 
-    header('Location:result/result.php');
-
-} elseif (isset($_SESSION['kokosnott'])){
-
-    header('Location:result/result.php');
-
-} elseif (isset($_SESSION['giraff'])){
-
-    header('Location:result/result.php');
-} else {
-    header('Location:settings.php');
 }
+
+if (isset($_SESSION['tiger'])){
+    header('Location:result/result.php');
+    exit;
+
+}
+if (isset($_SESSION['kokosnott'])){
+    header('Location:result/result.php');
+    exit;
+
+}
+if (isset($_SESSION['giraff'])){
+    header('Location:result/result.php');
+    exit;
+
+} 
+//When this should go to settings;
+if(!isset($_SESSION['apa'])) {
+    echo 
+    header('Location:settings.php');
+    exit;
+    
+}
+if(!isset($_SESSION['tiger'])) {
+    echo 
+    header('Location:settings.php');
+    exit;
+    
+}
+if(!isset($_SESSION['kokosnott'])) {
+    echo 
+    header('Location:settings.php');
+    exit;
+    
+}
+if(!isset($_SESSION['giraff'])) {
+    echo 
+    header('Location:settings.php');
+    exit;
+    
+}
+
 
 
 
