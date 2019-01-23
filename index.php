2972be65c67f7c277471e072f207ff0f1fef7275
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-23 22:02:30 +0100

    restucted

--- index.php
+++ index.php
@@ -1,54 +1,11 @@
 <?php
 session_start();
-if(isset($_SESSION['apa'])){
+if(isset($_SESSION['animal'])){
     header('Location:result/result.php');
     exit;
 
-}
-
-if (isset($_SESSION['tiger'])){
-    header('Location:result/result.php');
-    exit;
-
-}
-if (isset($_SESSION['kokosnott'])){
-    header('Location:result/result.php');
-    exit;
-
-}
-if (isset($_SESSION['giraff'])){
-    header('Location:result/result.php');
-    exit;
-
-} 
-//When this should go to settings;
-if(!isset($_SESSION['apa'])) {
-    echo 
+}else{
     header('Location:settings.php');
     exit;
-    
 }
-if(!isset($_SESSION['tiger'])) {
-    echo 
-    header('Location:settings.php');
-    exit;
-    
-}
-if(!isset($_SESSION['kokosnott'])) {
-    echo 
-    header('Location:settings.php');
-    exit;
-    
-}
-if(!isset($_SESSION['giraff'])) {
-    echo 
-    header('Location:settings.php');
-    exit;
-    
-}
-
-
-
-
-
 ?>
