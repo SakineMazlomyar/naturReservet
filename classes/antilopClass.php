5b1c213abd85b4748278f5191dfdc52fb32e7893
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-30 13:53:55 +0100

    addedClass

---
+++ classes/antilopClass.php
@@ -0,0 +1,14 @@
+<?php
+class Antilop extends Animal{
+    function __construct($animalPicSrc, $animalNmbers){
+        parent::__construct($animalPicSrc, $animalNmbers);
+    
+    }
+    function makeSound(){
+        for($i = 0; $i <$this->animalNmbers; $i++){
+            echo '<img style="width: 30%;" src='.$this->animalPicSrc.' onclick=playAntilopeSound()>';
+        }
+        
+    }
+}
+?>
---
+++ classes/granClass.php
@@ -0,0 +1,15 @@
+<?php
+class Gran extends Animal{
+    function __construct($animalPicSrc, $animalNmbers){
+        parent::__construct($animalPicSrc, $animalNmbers);
+    
+    }
+    function makeSound(){
+        for($i = 0; $i <$this->animalNmbers; $i++){
+            echo '<img style="width: 30%;" src='.$this->animalPicSrc.'>';
+        }
+        
+    }
+}
+
+?>
