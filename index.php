791ac46f935e9e2f6425ed0bfc4b88985a3dc5ff
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-31 10:13:22 +0100

    addedDie

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
+
 session_start();
 //When there is no session
 if(!isset($_SESSION['post'])) {
-
     header('Location:settings.php');
-    session_write_close();
-    exit();
-
+    die();
+ 
 }
 //When there is session
 if(isset($_SESSION['post'])) {
+    
     header('Location:result.php');
-    session_write_close();
-    exit();
+    die();
+
 }
+
+
+
+
 ?>
