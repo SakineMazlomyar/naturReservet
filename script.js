df17b725f1ac97e6b3320a6180f047faa749cd21
SakineMazlomyar <mazlomyar12@gmail.com>
2019-02-03 14:33:08 +0100

    addedNewFunc

--- script.js
+++ script.js
@@ -50,4 +50,40 @@ function playTigerSound(){
     var s =Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
     alert(s)
 }
+function playLionSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","./sounds/lion.mp3" )
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    var s =Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    alert(s)
+}
+function playGorillSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","./sounds/gorilla.mp3" )
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    var s =Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    alert(s)
+}
+function playAntelopeSound(){
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls")
+    var src = document.createElement("source");
+    src.setAttribute("src","./sounds/antelope.mp3" )
+
+    audio.appendChild(src);
+
+    document.body.appendChild(audio)
+    var s =Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    alert(s)
+}
 
